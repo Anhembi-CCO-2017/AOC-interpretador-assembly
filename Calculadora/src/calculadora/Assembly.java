f17c2bae543c4d55ae168c2775518a7a3b0fe12e
Lucas Golino <lucasgolino@gmail.com>
2017-10-28 15:31:06 -0200

    finalizando logica de indentificacao de Assembly

--- Calculadora/src/calculadora/Assembly.java
+++ Calculadora/src/calculadora/Assembly.java
@@ -1,19 +1,25 @@
 package calculadora;
 
-/*
- * Primeiro identificar se existe caracteres especiais.
- * Splitar linhas
- * Depois procura um 'MOV' NA PRIMEIRA LINHA
- *  Se achar
- *      le o codigo linha por linha
- *      verificar se na variavel nao existe uma operacao
- *      verifica se é uma op valida (MOV, ADD, SUB, MPY, DIV)
- *          Se sim
- *             verifica os operadores ( X,B) ESPACO,VARIAVEL,VARIAVEL
- *             se tudo certo, move para matriz [OP:0, VARIAVEL:1, VARIAVEL:2]
- *             garante que é um assembly retorna true;
- *          Se nao
- *              retorna false;
+/* v1: /(MOV|ADD|SUB|MPY|DIV) (\w),(\w)/g  $1 $2 != $3
+ * REGEX DE VALIDAÇÃO
+ * Primeiro identificar se existe caracteres especiais. [$&+:;=?!@#|]
+ *  Se houver
+ *      return false;
+ * Splitar linhas pelo \n usando SPLIT -> para vetor de (String linhas[]) com (String.split("\n"))
+ * Le Linha por linha com a pattern 'v1'
+ *  Se pattern reconhecida e se $2 != $3:
+        na primeira linha
+            $1 -> TEM QUE SER MOV;
+                se for
+                    salvar $1,$2,$3 na matriz [0][OP:0:$1, VARIAVEL1:1:$2, VARIAVEL2:2:$3]
+                se nao
+                    return false;
+        se nao for primeira linha
+            salvar $1,$2,$3 na matriz [linha][OP:0:$1, VARIAVEL1:1:$2, VARIAVEL2:2:$3]
+        No final se tudo verificado e salvo
+            return true;
+    Se não
+        return false;
  */
 public class Assembly {
     public String[][] matriz_operacao;
