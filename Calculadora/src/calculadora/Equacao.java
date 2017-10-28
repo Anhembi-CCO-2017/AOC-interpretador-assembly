e3b82af46c288ef95253138d5449269c409c5b10
Matheus Eli F <matheuselisouzaferreira@hotmail.com>
2017-10-28 14:01:32 -0200

    mudancas

--- Calculadora/src/calculadora/Equacao.java
+++ Calculadora/src/calculadora/Equacao.java
@@ -14,6 +14,7 @@ public class Equacao {
         for (int i = 0; i < valor.length(); i++) 
             for (int j = 0; j < vetor.length; j++)
                 if (valor.charAt(i) == vetor[j]){
+                    
                     if (valor.charAt(0) == vetor[j]) 
                         return false;
                     
