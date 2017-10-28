a6bccec5464fcf319f9da2b25af382874389ce2e
Thiago Almeida <thiagoloal@gmail.com>
2017-10-28 14:51:42 -0200

    Adicionando entrada de dados

--- Calculadora/src/calculadora/Calculadora.java
+++ Calculadora/src/calculadora/Calculadora.java
@@ -1,16 +1,33 @@
 
 package calculadora;
 
+import javax.swing.JOptionPane;
+import javax.swing.JScrollPane;
+import javax.swing.JTextArea;
+
 public class Calculadora {
 
     public static void main(String[] args) {
-
+        
+        // entrada de dados
+        JTextArea msg = new JTextArea("");
+        
+        msg.setLineWrap(true); //quebrar linhas
+        msg.setRows(10); //numero de linhas
+        msg.setColumns(10); //numero de colunas
+        msg.setWrapStyleWord(true); // qubrar paçavras
+        
+        // adicionando scroll na entrada de dados
+        JScrollPane scrollPane = new JScrollPane(msg);
+        
+        // abrindo dialogo
+        JOptionPane.showMessageDialog(null, scrollPane);
+        
+        String valor = msg.getText();
+        
+        // Validar a expressão        
         IdentificarExpressao ie = new IdentificarExpressao();
-        
-        String num = "! 9";
-        
-        System.out.println(ie.isEquacao(num));
-        
+               
         
     }
     
