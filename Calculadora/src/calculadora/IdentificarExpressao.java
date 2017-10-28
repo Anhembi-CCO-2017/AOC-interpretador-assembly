16aa55326e62a1d5e6a71c63bc40c5fc6769be2f
Lucas Golino <lucasgolino@gmail.com>
2017-10-28 14:03:54 -0200

    fix IdentificarExpressao class

--- Calculadora/src/calculadora/IdentificarExpressao.java
+++ Calculadora/src/calculadora/IdentificarExpressao.java
@@ -1,31 +1,25 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package calculadora;
 
-/**
- *
- * @author Matheus
- */
 public class IdentificarExpressao {
-    private String[][] matriz_operacao;
-    
+
     public IdentificarExpressao() {
     }
     
     public boolean isAssembly(String valor){
+        Assembly eq = new Assembly();
+
+        if(eq.isAssembly(valor))
+            return true;
+
         return false;
     }
     
     public boolean isEquacao(String valor){
         Equacao eq = new Equacao();
         
-        if (eq.isEquacao(valor)) {
-            
-          return true;  
-        }
+        if (eq.isEquacao(valor))
+            return true;
+
         return false;
     }
 }
