a87b4c7eb10d0847b6fb39bee6c997137946ddd3
Thiago Almeida <thiagoloal@gmail.com>
2017-10-28 15:30:12 -0200

    é equação!!!!!!!!!!!!!!

--- Calculadora/src/calculadora/Equacao.java
+++ Calculadora/src/calculadora/Equacao.java
@@ -13,25 +13,41 @@ public class Equacao {
     public boolean isEquacao(String valor){
 
         char[] vetor = {'/','*','+','-'};
-        Pattern regex = Pattern.compile("[$&+,:;=?!@#|]");
+        Pattern regex = Pattern.compile("[$&,:;=?!@#|]");
         Matcher matcher = regex.matcher(valor);
+        
+        System.out.println("Check 1");
+        if (matcher.find()) {
+            return false;
+        }
         
         Pattern regex1 = Pattern.compile("[/*+-]");
         Matcher matcher1 = regex1.matcher(valor);
-         
-        for (int j = 0; j < vetor.length; j++)
-                if (valor.charAt(0) == vetor[j]) 
-                        return false;
         
+        Pattern regex2 = Pattern.compile("[a-zA-Z]");
+        Matcher matcher3 = regex2.matcher(valor);
         
-        if (matcher.find() == false)
-            
-            return false;
+        Pattern regex3 = Pattern.compile("[0-9]");
+        Matcher matcher4 = regex3.matcher(valor);
         
-        
-        if (matcher1.find())
+        if(matcher1.find()){
+            if (matcher4.find()) {
+                return false;
+            }
             return true;
+        }
         
+        System.out.println("Check 2");
+        for (int j = 0; j < vetor.length; j++){
+            if (valor.charAt(0) == vetor[j] || valor.charAt(valor.length() - 1) == vetor[j]){
+                return false;
+            }
+        }
+        
+        System.out.println("Check 3");
+        if (matcher1.find()) {
+            return true;
+        }
         
         return false;
     }
