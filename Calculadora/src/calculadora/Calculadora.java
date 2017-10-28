727b14827872decc48fbc702b2aebc574f9e3583
Thiago Almeida <thiagoloal@gmail.com>
2017-10-28 11:54:59 -0200

    add thins

--- Calculadora/src/calculadora/Calculadora.java
+++ Calculadora/src/calculadora/Calculadora.java
@@ -4,9 +4,17 @@ package calculadora;
 public class Calculadora {
 
     public static void main(String[] args) {
+<<<<<<< HEAD
 
         System.out.println("calculadora");
         System.out.println("calculadora 3");
+=======
+        
+        
+        System.out.println("calculadora");
+        System.out.println("calculadora2");
+        
+        
 >>>>>>> fba2dc08eb5aeb1892b1065b55ed1bc5c4401893
     }
     
