05ca4c19d83fd0a98c2c7aead5edaf9c66c67734
Matheus Eli F <matheuselisouzaferreira@hotmail.com>
2017-10-28 14:57:16 -0200

    Mudancas no IsEquacao

--- Calculadora/src/calculadora/Equacao.java
+++ Calculadora/src/calculadora/Equacao.java
@@ -12,20 +12,26 @@ public class Equacao {
     
     public boolean isEquacao(String valor){
 
+        char[] vetor = {'/','*','+','-'};
         Pattern regex = Pattern.compile("[$&+,:;=?!@#|]");
         Matcher matcher = regex.matcher(valor);
-        System.out.println(matcher.find());
-        char[] vetor = {'/','*','+','-'};
         
-        for (int i = 0; i < valor.length(); i++) 
-            for (int j = 0; j < vetor.length; j++)
-                if (valor.charAt(i) == vetor[j]){
-                    
-                    if (valor.charAt(0) == vetor[j]) 
+        Pattern regex1 = Pattern.compile("[/*+-]");
+        Matcher matcher1 = regex1.matcher(valor);
+         
+        for (int j = 0; j < vetor.length; j++)
+                if (valor.charAt(0) == vetor[j]) 
                         return false;
-                    
-                    return true;
-                }
+        
+        
+        if (matcher.find() == false)
+            
+            return false;
+        
+        
+        if (matcher1.find())
+            return true;
+        
         
         return false;
     }
